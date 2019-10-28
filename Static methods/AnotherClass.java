db48a129d764876fed1e81ce6f4a4ebc76229f41
sumutoni <54947406+sumutoni@users.noreply.github.com>
2019-10-28 09:40:02 -0400

    Add files via upload

---
+++ Static methods/AnotherClass.java
@@ -0,0 +1,9 @@
+
+public class AnotherClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Static methods/MethodDemo.java
@@ -0,0 +1,12 @@
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+		add(5,3);
+	}
+	public static void add(int a, int b) {
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
